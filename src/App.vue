2bed93475809413de860c064f4e0c2602ff3134c
anonymousvegan <majjunjulavgust@gmail.com>
2024-05-18 23:47:23 +0200

    Update basic styles

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,19 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterView />
+	<RouterView />
 </template>
+
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap');
+
+*,
+*::before,
+*::after {
+	font-family: "Outfit", sans-serif;
+	font-optical-sizing: auto;
+	margin: 0;
+	padding: 0;
+}
+</style>
